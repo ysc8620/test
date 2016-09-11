64c818510eac015c0bc178a58c5ef2b523b34543
ysc8620 <ysc8620@163.com>
2016-09-11 21:14:30 +0800

    ds

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,15 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+
+
+    'DB_TYPE' => 'mysql',
+    'DB_CHARSET'=> 'utf8mb4',
+    'DB_HOST' => '127.0.0.1',
+    'DB_NAME' => 'test',
+    'DB_USER' => 'root',
+    'DB_PWD' => '123456',
+    //  'DB_RW_SEPARATE'=>true,
+    'DB_PREFIX' => 't_',
+    'DB_PORT' => '3306'
 );
