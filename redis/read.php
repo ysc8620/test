9aa2700d67cb597c23946bd859caeedd051f4bab
ysc8620 <ysc8620@163.com>
2016-09-11 11:50:56 +0800

    ds

---
+++ redis/read.php
@@ -0,0 +1,90 @@
+<?php
+$id = intval($argv[1]);
+
+$redis = new redis();
+$redis->connect("127.0.0.1",6379);
+echo 'user_read_'.$id."\r\n";
+$bool = $redis->get('user_read_'.$id);
+$redis->watch('user_read_'.$id);
+#sleep(3);
+if(
+!$bool
+){
+    $redis->multi();
+    $redis->incr('user_read_'.$id, 1);
+    $redis->incrByFloat('user_bank_'.$id, 10);
+    $res = $redis->exec();
+    if($res){
+        echo "ok===\r\n";
+    }else{
+        echo "exit===\r\n";
+    }
+}else{
+    echo "false\r\n";
+}
+
+/////////////6380
+$redis->connect("127.0.0.1",6380);
+echo 'user_read_'.$id."\r\n";
+$bool = $redis->get('user_read_'.$id);
+$redis->watch('user_read_'.$id);
+#sleep(3);
+if(
+!$bool
+){
+    $redis->multi();
+    $redis->incr('user_read_'.$id, 1);
+    $redis->incrByFloat('user_bank_'.$id, 10);
+    $res = $redis->exec();
+    if($res){
+        echo "ok===\r\n";
+    }else{
+        echo "exit===\r\n";
+    }
+}else{
+    echo "false\r\n";
+}
+
+/////////////6381
+$redis->connect("127.0.0.1",6381);
+echo 'user_read_'.$id."\r\n";
+$bool = $redis->get('user_read_'.$id);
+$redis->watch('user_read_'.$id);
+#sleep(3);
+if(
+!$bool
+){
+    $redis->multi();
+    $redis->incr('user_read_'.$id, 1);
+    $redis->incrByFloat('user_bank_'.$id, 10);
+    $res = $redis->exec();
+    if($res){
+        echo "ok===\r\n";
+    }else{
+        echo "exit===\r\n";
+    }
+}else{
+    echo "false\r\n";
+}
+
+/////////////6382
+$redis->connect("127.0.0.1",6382);
+echo 'user_read_'.$id."\r\n";
+$bool = $redis->get('user_read_'.$id);
+$redis->watch('user_read_'.$id);
+#sleep(3);
+if(
+!$bool
+){
+    $redis->multi();
+    $redis->incr('user_read_'.$id, 1);
+    $redis->incrByFloat('user_bank_'.$id, 10);
+    $res = $redis->exec();
+    if($res){
+        echo "ok===\r\n";
+    }else{
+        echo "exit===\r\n";
+    }
+}else{
+    echo "false\r\n";
+}
