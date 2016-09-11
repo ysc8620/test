634d9243c06e441ea53ec1455ed0f93525862012
ysc8620 <ysc8620@163.com>
2016-09-11 22:02:02 +0800

    ds

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,14 +2,14 @@
 return array(
 	//'配置项'=>'配置值'
 
-
+    'DB_DEPLOY_TYPE'=> true,
     'DB_TYPE' => 'mysql',
     'DB_CHARSET'=> 'utf8mb4',
-    'DB_HOST' => '127.0.0.1',
+    'DB_HOST' => '192.168.92.128,192.168.92.129',
     'DB_NAME' => 'test',
-    'DB_USER' => 'root',
-    'DB_PWD' => '123456',
-    //  'DB_RW_SEPARATE'=>true,
+    'DB_USER' => 'root,root',
+    'DB_PWD' => '123456,123456',
+     'DB_RW_SEPARATE'=>true,
     'DB_PREFIX' => 't_',
     'DB_PORT' => '3306'
 );
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,7 +3,11 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+        echo time()."\r\n";
+//        $t = M('test')->add(['name'=>'test','status'=>1,'create_time'=>time()]);
+//       var_dump($t);
         $re = M('test')->find();
+        var_dump(M()->getDbFields());
         var_dump($re);
     }
 }
