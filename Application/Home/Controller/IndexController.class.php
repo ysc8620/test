f670c1f497be179e06774aac209da8035dd5495f
ysc8620 <ysc8620@163.com>
2016-09-11 22:03:51 +0800

    ds

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+        echo $_SERVER["SERVER_NAME"]."<br/>";
         echo time()."\r\n";
 //        $t = M('test')->add(['name'=>'test','status'=>1,'create_time'=>time()]);
 //       var_dump($t);
