4c5552960888893631fb60e21d6ae5eb85aaafcb
ShengYue <ysc8620@163.com>
2016-09-12 11:07:31 +0800

    ds

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -5,10 +5,10 @@ return array(
     'DB_DEPLOY_TYPE'=> true,
     'DB_TYPE' => 'mysql',
     'DB_CHARSET'=> 'utf8mb4',
-    'DB_HOST' => '192.168.113.128,192.168.113.129',
+    'DB_HOST' => '192.168.113.128,192.168.113.129,192.168.113.129',
     'DB_NAME' => 'test',
-    'DB_USER' => 'root,root',
-    'DB_PWD' => '123456,123456',
+    'DB_USER' => 'root',
+    'DB_PWD' => '123456',
      'DB_RW_SEPARATE'=>true,
     'DB_PREFIX' => 't_',
     'DB_PORT' => '3306'
