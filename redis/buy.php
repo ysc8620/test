10b88e8f62f9bca0cf0c4ebff88869f5af39e2ff
ShengYue <ysc8620@163.com>
2016-09-12 16:42:24 +0800

    ds

---
+++ redis/buy.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ShengYue
+ * Date: 2016/9/12
+ * Time: 16:16
+ */
+// $yonghu_yue
+
+$redis->decr();
+
+$id = intval($argv[1]);
+
+$redis = new redis();
+$redis->connect("192.168.92.128",6379);
+# echo 'user_read_'.$id."\r\n";
+$bool = $redis->get('user_read_'.$id);
+
+$goods_price = $redis->get("goods:price:100");
+$user_bank = $redis->get("user:bank:100");
+$goods_sku = $redis->get("goods:sku:100");
+
+if($user_bank < $goods_price){
+    echo "user bank slow";
+    die();
+}
+
+if($goods_sku<1){
+    echo "goods sku slow";
+    die();
+}
+
+$redis->watch("user:bank:100");
+$redis->watch("goods:price:100");
+$redis->multi();
+$res1 = $redis->incrByFloat("user:bank:100","-{$goods_price}");
+$res2 = $redis->decr("goods:sku:100");
+
+$res3 = $redis->exec();
+if($res3){
+    echo "ok
+\n";
+}else{
+    echo "false\r\n";
+}
